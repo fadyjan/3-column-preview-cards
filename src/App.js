08a2f10e6c1f592ab0d31b76ab444d4341ab233b
fadyjan <fady.jan95@gmail.com>
2023-03-09 22:14:32 +0200

    Retouches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import LuxIcon from "./assets/icon-luxury.svg";
+import SedanIcon from "./assets/icon-sedans.svg";
+import SuvsIcon from "./assets/icon-suvs.svg";
+import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
+  const CarsObject = [
+    {
+      Tittle: "SEDANS",
+      Icon: SedanIcon,
+      CarDescription:
+        "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    },
+    {
+      Tittle: "SUVS",
+      Icon: SuvsIcon,
+      CarDescription:
+        "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    },
+    {
+      Tittle: "LUXURY",
+      Icon: LuxIcon,
+      CarDescription:
+        "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <div id="MainContainer">
+        {CarsObject.map((car) => {
+          return <Card carRecord={car}></Card>;
+        })}
+      </div>
     </div>
   );
 }
